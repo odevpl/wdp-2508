13b56fae90c6f08547799bb7ae33cd82e82c9b5a
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-08-14 17:07:57 +0200

    add: count state to ComapnyClaim.js

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -5,34 +5,40 @@ import styles from './CompanyClaim.module.scss';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMobileAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
-const CompanyClaim = () => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className='row align-items-center'>
-        <div className={`col text-left ${styles.phoneNumber}`}>
-          <p>
-            <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
-            222
-          </p>
-        </div>
-        <div className='col text-center'>
-          <a href='#'>
-            <img src='/images/logo.png' alt='Bazar' />
-          </a>
-        </div>
-        <div className={`col text-right ${styles.cart}`}>
-          <a href='#' className={styles.cartBox}>
-            <div className={styles.cartIcon}>
-              <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
-            </div>
-            <div className={styles.cartCounter}>0</div>
-          </a>
+const CompanyClaim = () => {
+  const [count, setCount] = useState(1);
+  const showCount = count > 99999 ? '99999+' : count;
+
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className='row align-items-center'>
+          <div className={`col text-left ${styles.phoneNumber}`}>
+            <p>
+              <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560
+              - 222
+            </p>
+          </div>
+          <div className='col text-center'>
+            <a href='#'>
+              <img src='/images/logo.png' alt='Bazar' />
+            </a>
+          </div>
+          <div className={`col text-right ${styles.cart}`}>
+            <a href='#' className={styles.cartBox}>
+              <div className={styles.cartIcon}>
+                <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
+              </div>
+              <div className={styles.cartCounter}>{showCount}</div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 // CompanyClaim.propTypes = {};
 
