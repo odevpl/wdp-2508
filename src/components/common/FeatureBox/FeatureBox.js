410d51407dc9da3606d51ab4ca67ea1b11ce77a4
Mateusz Matysek <mateusziom1@gmail.com>
2025-08-14 10:11:11 +0200

    Change 'div' to 'Link' in FeatureBoxes.js

--- src/components/common/FeatureBox/FeatureBox.js
+++ src/components/common/FeatureBox/FeatureBox.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './FeatureBox.module.scss';
 
-const FeatureBox = ({ active, icon, children }) => (
+const FeatureBox = ({ active, icon, children, link }) => (
   <div className={styles.root + (active ? ' ' + styles.active : '')}>
     {icon && (
       <div className={styles.iconWrapper}>
         <FontAwesomeIcon className={styles.icon} icon={icon} />
       </div>
     )}
-    <div className={styles.content}>{children}</div>
+    <Link to={link} className={styles.content}>
+      {children}
+    </Link>
   </div>
 );
 
@@ -20,6 +22,7 @@ FeatureBox.propTypes = {
   children: PropTypes.node,
   icon: PropTypes.object,
   active: PropTypes.bool,
+  link: PropTypes.string,
 };
 
 export default FeatureBox;
