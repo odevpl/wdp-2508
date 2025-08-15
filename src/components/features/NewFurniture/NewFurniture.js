f6bc44e989ec62304092095b20545f92bc3a8f81
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-08-15 19:33:35 +0200

    add: handleCompare prop to ProductBox.js, update propType, added onClick to Button

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -87,7 +87,7 @@ class NewFurniture extends React.Component {
           <div className='row'>
             {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map(item => (
               <div key={item.id} className='col-3'>
-                <ProductBox {...item} />
+                <ProductBox {...item} handleCompare={() => this.handleCompare(item)} />
               </div>
             ))}
           </div>
