30aafc250a9067c702eeb7741e6f2bb5eb659fd1
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-08-16 16:46:17 +0200

    add: dropbar in MenuBar.js for mobile screen

--- src/components/layout/MenuBar/MenuBar.js
+++ src/components/layout/MenuBar/MenuBar.js
@@ -1,48 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import ProductSearch from '../../features/ProductSearch/ProductSearch';
 
 import styles from './MenuBar.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-const MenuBar = ({ children }) => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className='row align-items-center'>
-        <div className='col'>
-          <ProductSearch />
-        </div>
-        <div className={'col-auto ' + styles.menu}>
-          <ul>
-            <li>
-              <a href='#' className={styles.active}>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href='#'>Furniture</a>
-            </li>
-            <li>
-              <a href='#'>Chair</a>
-            </li>
-            <li>
-              <a href='#'>Table</a>
-            </li>
-            <li>
-              <a href='#'>Sofa</a>
-            </li>
-            <li>
-              <a href='#'>Bedroom</a>
-            </li>
-            <li>
-              <a href='#'>Blog</a>
-            </li>
-          </ul>
+const MenuBar = ({ children }) => {
+  const [shown, setShown] = useState(false);
+
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className={styles.menuBar}>
+          <div className={styles.searchMenu}>
+            <ProductSearch />
+          </div>
+          <div className={styles.menu}>
+            <ul>
+              <li>
+                <a href='#' className={styles.active}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href='#'>Furniture</a>
+              </li>
+              <li>
+                <a href='#'>Chair</a>
+              </li>
+              <li>
+                <a href='#'>Table</a>
+              </li>
+              <li>
+                <a href='#'>Sofa</a>
+              </li>
+              <li>
+                <a href='#'>Bedroom</a>
+              </li>
+              <li>
+                <a href='#'>Blog</a>
+              </li>
+            </ul>
+          </div>
+          <div className={`dropdown ${styles.dropDown}`} tabIndex={0}>
+            <button
+              className='btn btn-secondary dropdown-toggle'
+              type='button'
+              onClick={() => setShown(!shown)}
+            >
+              <FontAwesomeIcon icon={faBars} />
+            </button>
+            {shown && (
+              <ul className='dropdown-menu show'>
+                <li>
+                  <a className='dropdown-item' href='#'>
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a className='dropdown-item' href='#'>
+                    Furniture
+                  </a>
+                </li>
+                <li>
+                  <a className='dropdown-item' href='#'>
+                    Chair
+                  </a>
+                </li>
+                <li>
+                  <a className='dropdown-item' href='#'>
+                    Table
+                  </a>
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 MenuBar.propTypes = {
   children: PropTypes.node,
