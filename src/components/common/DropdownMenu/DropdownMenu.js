50f8eb27df6467af43d3b3e07bb368fae9555494
Mateusz Matysek <mateusziom1@gmail.com>
2025-08-17 14:05:09 +0200

    Add 'DropdownMenu' component

---
+++ src/components/common/DropdownMenu/DropdownMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './DropdownMenu.module.scss';
+
+const DropdownMenu = () => {
+  return (
+    <ul>
+      <li>Select Categories</li>
+    </ul>
+  );
+};
+
+export default DropdownMenu;
--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faListUl, faSearch, faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import DropdownMenu from '../../common/DropdownMenu/DropdownMenu';
 
 import styles from './ProductSearch.module.scss';
 
@@ -10,7 +11,7 @@ const ProductSearch = () => (
   <form action='' className={styles.root}>
     <div className={styles.category}>
       <FontAwesomeIcon className={styles.icon} icon={faListUl} />
-
+      <DropdownMenu />
       <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
     </div>
     <div className={styles.searchField}>
