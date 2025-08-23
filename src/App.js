ff36325cb86ef5694ff2a3c06cbc14324468d970
Daniel Bednarczyk <d.bednarczyk.99@gmail.com>
2025-08-24 00:25:00 +0200

    Add Furniture page Route in App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => (
       <MainLayout>
         <Switch>
           <Route exact path={'/'} component={Homepage} />
-          <Route exact path={'/shop/:categoryId'} component={ProductList} />
+          <Route exact path={['/shop/:categoryId', '/shop']} component={ProductList} />
           <Route exact path={'/product/:productId'} component={ProductPage} />
           <Route exact path={'/blog'} component={BlogPage} />
         </Switch>
