e795843d1ae6566b052413b9c0e7feea43bcbdd5
Mateusz Matysek <mateusziom1@gmail.com>
2025-08-25 20:37:14 +0200

    Add Reviews to 'ProductPage.js'

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -20,10 +20,7 @@ import {
   faEnvelope,
   faPlus,
   faMinus,
-  faStar,
 } from '@fortawesome/free-solid-svg-icons';
-
-import { faStar as faStarOutline } from '@fortawesome/free-regular-svg-icons';
 import {
   faFacebookF,
   faTwitter,
@@ -218,6 +215,67 @@ const ProductPage = () => {
                 </ul>
               </div>
             </div>
+          </div>
+        </div>
+        <div className={`${styles.productTabs} ${styles.section}`}>
+          <div className={`${styles.tabs}`}>
+            <a href='#' className={`${styles.tab}`}>
+              Description
+            </a>
+            <a href='#' className={`${styles.tab} ${styles.active}`}>
+              Reviews{`(0)`}
+            </a>
+            <a href='#' className={`${styles.tab}`}>
+              Specification
+            </a>
+            <a href='#' className={`${styles.tab}`}>
+              Custom Tab
+            </a>
+          </div>
+          <div className={`${styles.tabContent}`}>
+            <form action='' className={`${styles.tabReview}`}>
+              <fieldset className={styles.reviews}>
+                {<p>There are no reviews yet.</p>}
+              </fieldset>
+              <fieldset className={styles.addReview}>
+                <label htmlFor='review'>Add a review</label>
+              </fieldset>
+              <fieldset className={styles.userRating}>
+                <p>Your Rating</p>
+                <div className={styles.stars}>
+                  <span>Bad</span>
+                  <StarRating stars={0} userStars={0} />
+                  <span>Good</span>
+                </div>
+              </fieldset>
+              <fieldset className={styles.userReview}>
+                <label htmlFor='review'>Your Review</label>
+                <textarea id='review' name='review' />
+                <div className={`${styles.userData} row no-gutters`}>
+                  <input
+                    className={`col`}
+                    type='text'
+                    id='name'
+                    name='name'
+                    placeholder='Name*'
+                  />
+                  <input
+                    className={`col`}
+                    type='email'
+                    id='email'
+                    name='email'
+                    placeholder='Email*'
+                  />
+                  <Button
+                    className={`${styles.submitBtn} col-2`}
+                    variant='small'
+                    role='submit'
+                  >
+                    Continue
+                  </Button>
+                </div>
+              </fieldset>
+            </form>
           </div>
         </div>
       </div>
