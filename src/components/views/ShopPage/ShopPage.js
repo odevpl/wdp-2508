69086c7a8952f773a7ec3b79327514bf8aa9bc3e
Daniel Bednarczyk <d.bednarczyk.99@gmail.com>
2025-08-28 23:39:51 +0200

    Add Banner.js to ShopPage.js

--- src/components/views/ShopPage/ShopPage.js
+++ src/components/views/ShopPage/ShopPage.js
@@ -5,6 +5,7 @@ import { faGripVertical, faList, faTimes } from '@fortawesome/free-solid-svg-ico
 import Button from '../../common/Button/Button';
 import ProductBox from '../../common/ProductBox/ProductBox';
 import PropTypes from 'prop-types';
+import Banner from '../../features/Banner/Banner';
 
 const ShopPage = ({ products, viewport }) => {
   const [activePage] = useState(0);
@@ -35,6 +36,9 @@ const ShopPage = ({ products, viewport }) => {
   return (
     <div className={styles.root}>
       <div className='container'>
+        <div className='row no-gutters'>
+          <Banner />
+        </div>
         <div className={styles.panelBar}>
           <div className={`row no-gutters align-items-end ${styles.panelBarMenu}`}>
             <div className={styles.heading}>
