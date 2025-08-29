cb0e25679fb068ee74c30f1bc664a1b5762bc8e2
Daniel Trzeszczkowski <daniel_trzeszczkowski@o2.pl.>
2025-08-29 21:40:23 +0200

    Dodanie walidacji na podstrone logowaniaa

--- src/components/views/LoginPage/LoginPage.test.js
+++ src/components/views/LoginPage/LoginPage.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
 import LoginPage from './LoginPage';
 
 describe('LoginPage', () => {
   it('renders without crashing', () => {
-    shallow(<LoginPage />);
+    shallow(
+      <MemoryRouter>
+        <LoginPage />
+      </MemoryRouter>
+    );
   });
 });
